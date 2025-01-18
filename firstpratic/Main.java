4c083f1c4f7cafe8c84b68a4aaa1910d9783f22f
Isabelly Lima <mslima5643@gmail.com>
2025-01-18 10:24:47 -0300

    up

---
+++ firstpratic/Main.java
@@ -0,0 +1,11 @@
+package firstpratic;
+
+public class Main {
+    public static void main(String[] args) {
+        int y = 32;
+        double x = 10.258;
+        System.out.println("bom dia");
+        System.out.println(y);
+        System.out.printf("%.4f\n",x);
+}
+}
