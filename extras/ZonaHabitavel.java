f0ca468db3e0cbc178be4fccc02eb3e122de9180
Isabelly Lima <mslima5643@gmail.com>
2025-02-20 15:55:55 -0300

    up

--- extras/ZonaHabitavel.java
+++ extras/ZonaHabitavel.java
@@ -15,7 +15,7 @@ public class ZonaHabitavel {
 
         sc.close();
 
-        // Intervalos da Zona Habitável (em UA)
+      
         float limiteInferior = 0, limiteSuperior = 0;
         
         switch (tipoEstrela) {
@@ -23,20 +23,20 @@ public class ZonaHabitavel {
             case 'B': limiteInferior = 1.5f; limiteSuperior = 10.0f; break;
             case 'A': limiteInferior = 0.8f; limiteSuperior = 3.0f; break;
             case 'F': limiteInferior = 0.5f; limiteSuperior = 2.0f; break;
-            case 'G': limiteInferior = 0.8f; limiteSuperior = 1.5f; break; // Exemplo: Sol
+            case 'G': limiteInferior = 0.8f; limiteSuperior = 1.5f; break; // sol
             case 'K': limiteInferior = 0.4f; limiteSuperior = 0.9f; break;
-            case 'M': limiteInferior = 0.1f; limiteSuperior = 0.4f; break; // Exemplo: Anã Vermelha
+            case 'M': limiteInferior = 0.1f; limiteSuperior = 0.4f; break; // Anã Vermelha
             default:
                 System.out.println("Tipo de estrela inválido! Use O, B, A, F, G, K ou M.");
                 return;
         }
 
         if (distancia >= limiteInferior && distancia <= limiteSuperior) {
-            System.out.println("🌍 O planeta está na zona habitável! Pode ter água líquida!");
+            System.out.println("O planeta está na zona habitável! Pode ter água líquida!");
         } else if (distancia < limiteInferior) {
-            System.out.println("🔥 O planeta está muito perto da estrela! Muito quente!");
+            System.out.println("O planeta está muito perto da estrela! Muito quente!");
         } else {
-            System.out.println("❄️ O planeta está muito longe da estrela! Muito frio!");
+            System.out.println(" O planeta está muito longe da estrela! Muito frio!");
         }
     }
 }
